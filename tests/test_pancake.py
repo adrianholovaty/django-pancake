d5ea7321fe9c6289179f3cec760fc7cd8969cf0c
Adrian Holovaty <adrian@holovaty.com>
2012-02-01 13:18:30 -0600

    Fixed two bugs in the unit tests

--- tests/test_pancake.py
+++ tests/test_pancake.py
@@ -9,8 +9,8 @@ TESTS = {
     'child1': ('{% extends "base1" %} {% block title %}It worked{% endblock %}', '<html><title>It worked</title></html>'),
 
     # {% load %} statements get bubbled up and combined.
-    'loadbase1': ('{% load webdesign %}<html><title>{% block title %}</title></html>', '{% load webdesign %}<html><title></title></html>'),
-    'load1': ('{% extends "base1" %}{% load humanize %}{% block title %}Load 1{% endblock %}', '{% load humanize %}<html><title>It worked</title></html>'),
+    'loadbase1': ('{% load webdesign %}<html><title>{% block title %}{% endblock %}</title></html>', '{% load webdesign %}<html><title></title></html>'),
+    'load1': ('{% extends "base1" %}{% load humanize %}{% block title %}Load 1{% endblock %}', '{% load humanize %}<html><title>Load 1</title></html>'),
     'load2': ('{% extends "loadbase1" %}{% load humanize %}{% block title %}Load 2{% endblock %}', '{% load humanize webdesign %}<html><title>Load 2</title></html>'),
 }
 TEMPLATES = dict((k, v[0]) for k, v in TESTS.items())
