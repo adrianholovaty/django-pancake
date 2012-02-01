263699eae24c80e91a680372c4ed95130b2eda09
Adrian Holovaty <adrian@holovaty.com>
2012-02-01 12:39:23 -0600

    More work

--- django_pancake/flatten.py
+++ django_pancake/flatten.py
@@ -2,15 +2,32 @@ from django.template.base import Lexer
 import os
 import re
 
-class ParsedTemplate(object):
+class ASTNode(object):
+    "A node in the AST."
     def __init__(self, name):
         self.name = name
-        self.parent = None # A ParsedTemplate object, or None if there's no parent.
-        self.blocks = {}
-        self.content = []
+        self.children = [] # Each child can be a string or another ASTNode.
 
     def __repr__(self):
-        return '<ParsedTemplate: %r>' % self.name
+        return '<%s %s>' % (self.__class__.__name__, self.name)
+
+    def sub_nodes(self):
+        for child in self.children:
+            if isinstance(child, ASTNode):
+                yield child
+                for subnode in child.sub_nodes():
+                    yield subnode
+
+class Template(ASTNode):
+    "Root node of the AST. Represents a template, which may or may not have a parent."
+    def __init__(self, name):
+        super(Template, self).__init__(name)
+        self.parent = None # Template object for the parent template, if there's a parent.
+        self.blocks = {} # Maps block names to objects in self.children for quick lookup.
+
+class Block(ASTNode):
+    "Represents a {% block %}."
+    pass
 
 class TemplateDirectory(object):
     "Dictionary-like object that maps template names to template strings."
@@ -24,22 +41,21 @@ class TemplateDirectory(object):
 class Parser(object):
     def parse(self, template_name, templates):
         """
-        Returns a ParsedTemplate object, handling template inheritance.
+        Creates an AST for the given template. Returns a Template object.
         """
         self.templates = templates # Maps template names to template sources.
-        self.result = ParsedTemplate(template_name)
-        self.current_block_name = None
-        self.current_block = []
-
+        self.root = Template(template_name)
+        self.stack = [self.root]
+        self.current = self.root
         self.tokens = Lexer(templates[template_name], 'django-pancake').tokenize()
         while self.tokens:
             token = self.next_token()
 
             if token.token_type == 0: # TOKEN_TEXT
-                self.current_block.append(token.contents)
+                self.current.children.append(token.contents)
 
             elif token.token_type == 1: # TOKEN_VAR
-                self.current_block.append('{{ %s }}' % token.contents)
+                self.current.children.append('{{ %s }}' % token.contents)
 
             elif token.token_type == 2: # TOKEN_BLOCK
                 try:
@@ -50,11 +66,9 @@ class Parser(object):
                 if hasattr(self, method_name):
                     getattr(self, method_name)(arg)
                 else:
-                    self.current_block.append('{%% %s %%}' % token.contents)
+                    self.current.children.append('{%% %s %%}' % token.contents)
 
-        if self.current_block:
-            self.result.content = self.current_block
-        return self.result
+        return self.root
 
     def next_token(self):
         return self.tokens.pop(0)
@@ -62,24 +76,25 @@ class Parser(object):
     def do_block(self, text):
         if not text:
             raise ValueError('{% block %} without a name')
-        self.current_block_name = text
+        self.current.children.append(Block(text))
+        self.root.blocks[text] = self.current = self.current.children[-1]
+        self.stack.append(self.current)
 
     def do_endblock(self, text):
-        self.result.blocks[self.current_block_name] = self.current_block
-        self.current_block_name = None
-        self.current_block = []
+        self.stack.pop()
+        self.current = self.stack[-1]
 
     def do_extends(self, text):
         if not text:
             raise ValueError('{% extends %} without an argument')
         if text[0] in ('"', "'"):
             parent_name = text[1:-1]
-            self.result.parent = Parser().parse(parent_name, self.templates)
+            self.root.parent = Parser().parse(parent_name, self.templates)
         else:
             raise ValueError('Variable {% extends %} tags not supported')
 
 def flatten_parsed_template(template):
-    "Given a ParsedTemplate object, returns a string of flattened template text."
+    "Given an AST as returned by the parser, returns a string of flattened template text."
     # First, make a list from the template inheritance structure -- the family.
     # This will be in order from broad to specific.
     family = []
@@ -89,13 +104,15 @@ def flatten_parsed_template(template):
             break
         template = template.parent
 
-    print family
-
-    # result = family[0]
-    # for name, value in result.blocks.items():
-    #     for other in family[1:]:
-    #         if name in family.blocks:
-    #             value = family.blocks[name]
+    # Now, starting with the base template, loop downward over the child
+    # templates (getting more specific). For each child template, fill in the
+    # blocks in the parent template.
+    result = family[0]
+    for child in family[1:]:
+        for block in child.sub_nodes():
+            if block.name in result.blocks:
+                result.blocks[block.name].children = block.children
+    return result
 
 def flatten(source, templates):
     f = Flattener()
@@ -103,7 +120,7 @@ def flatten(source, templates):
     return result
 
 if __name__ == "__main__":
-    #from django_pancake.flatten import Parser, TemplateDirectory
+    from django_pancake.flatten import Parser, TemplateDirectory, flatten_parsed_template
     p = Parser()
     tp = p.parse('newsitem_list/neighbor-events.html', TemplateDirectory('/Users/adrian/code/everyblock/everyblock/everyblock/templates/site'))
-    flatten_parsed_template(tp)
+    result = flatten_parsed_template(tp)
--- tests/test_pancake.py
+++ tests/test_pancake.py
@@ -5,8 +5,13 @@ TESTS = {
     'test1': ('Some text here', 'Some text here'),
     'test2': ('{% unknown_block_tag %}Text here', '{% unknown_block_tag %}Text here'),
 
-    'base1': ('<html><title>{% block title %}</title></html>', '<html><title></title></html>'),
+    'base1': ('<html><title>{% block title %}{% endblock %}</title></html>', '<html><title></title></html>'),
     'child1': ('{% extends "base1" %} {% block title %}It worked{% endblock %}', '<html><title>It worked</title></html>'),
+
+    # {% load %} statements get bubbled up and combined.
+    'loadbase1': ('{% load webdesign %}<html><title>{% block title %}</title></html>', '{% load webdesign %}<html><title></title></html>'),
+    'load1': ('{% extends "base1" %}{% load humanize %}{% block title %}Load 1{% endblock %}', '{% load humanize %}<html><title>It worked</title></html>'),
+    'load2': ('{% extends "loadbase1" %}{% load humanize %}{% block title %}Load 2{% endblock %}', '{% load humanize webdesign %}<html><title>Load 2</title></html>'),
 }
 templates = dict((k, v[0]) for k, v in TESTS.items())
 
