a9e6d55bd0aef2f5cf22fc3f34079d2789f716e7
Adrian Holovaty <adrian@holovaty.com>
2012-02-01 13:27:03 -0600

    Added more tests

--- tests/test_pancake.py
+++ tests/test_pancake.py
@@ -1,12 +1,31 @@
 from django_pancake.flatten import flatten
 
+# template_name: (template_source, pancake_source),
 TESTS = {
-    # template_name: (template_source, pancake_source),
+    # No inheritance.
     'test1': ('Some text here', 'Some text here'),
     'test2': ('{% unknown_block_tag %}Text here', '{% unknown_block_tag %}Text here'),
 
+    # Basic inheritance.
     'base1': ('<html><title>{% block title %}{% endblock %}</title></html>', '<html><title></title></html>'),
     'child1': ('{% extends "base1" %} {% block title %}It worked{% endblock %}', '<html><title>It worked</title></html>'),
+
+    # Junk in child templates.
+    'junk1': ('{% extends "base1" %} Junk goes here', '<html><title></title></html>'),
+    'junk2': ('{% extends "base1" %} Junk goes here {% some_tag %}', '<html><title></title></html>'),
+    'junk3': ('{% extends "base1" %} Junk goes here {% some_tag %}  {% block title %}Worked{% endblock %}', '<html><title>Worked</title></html>'),
+
+    # Inheritance with default values in blocks.
+    'defaultbase1': ('<title>{% block title %}Default{% endblock %}</title>', '<title>Default</title>'),
+    'default1': ('{% extends "defaultbase1" %}', '<title>Default</title>'),
+    'default2': ('{% extends "defaultbase1" %}{% block title %}No default{% endblock %}', '<title>No default</title>'),
+
+    # Blocks within blocks.
+    'withinbase1': ('<title>{% block fulltitle %}{% block title %}Welcome{% endblock %} | Example.com{% endblock %}</title>', '<title>Welcome | Example.com</title>'),
+    'within1': ('{% extends "withinbase1" %}  {% block fulltitle %}Yay{% endblock %}', '<title>Yay</title>'),
+    'within2': ('{% extends "withinbase1" %}  {% block title %}Some page{% endblock %}', '<title>Some page | Example.com</title>'),
+
+    # Three-level inheritance structure.
 
     # {% load %} statements get bubbled up and combined.
     'loadbase1': ('{% load webdesign %}<html><title>{% block title %}{% endblock %}</title></html>', '{% load webdesign %}<html><title></title></html>'),
