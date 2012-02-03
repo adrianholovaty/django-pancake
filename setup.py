58b6c35f585da993e0774a884c5a5893c576c46c
Adrian Holovaty <adrian@holovaty.com>
2012-02-03 15:50:11 -0600

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='django-pancake',
+    version='0.1',
+    description='Library for "flattening" Django templates.',
+    author='Adrian Holovaty',
+    author_email='adrian@holovaty.com',
+    url='https://github.com/adrianholovaty/django-pancake',
+    license='MIT',
+    packages=['django_pancake'],
+)
